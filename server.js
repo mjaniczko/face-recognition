a5abe1da39504e660edf420a5705b6c21a354b95
Michał Janiczko <michaljaniczko1@gmail.com>
2019-05-28 21:46:14 +0200

    connection var name fix

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image')
 const db = knex({
   client: 'pg',
   connection: {
-    host : process.env.DATABASE_URL,
+    connectionString : process.env.DATABASE_URL,
     ss1: true,
   }
 })
