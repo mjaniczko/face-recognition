8cb1c653c9e19683982cbe6437062cfac2d5c640
Michał Janiczko <michaljaniczko1@gmail.com>
2019-05-28 21:38:33 +0200

    Add database link

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image')
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-concave-55156',
     user : 'postgres',
     password : 'password',
     database : 'smart-brain'
